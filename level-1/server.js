27dabada09ef511d8fac2a130d8d4a5900115ef3
ebakoba <kaarelallemann@gmail.com>
2018-11-12 11:51:32 +0200

    add login functionality

--- level-1/server.js
+++ level-1/server.js
@@ -1,11 +1,74 @@
 console.log('hello from server 1')
-
+const bcrypt = require('bcrypt')
+const fs = require('fs')
+const path = require('path')
+const Koa = require('koa')
+const session = require('koa-session')
+const bodyParser = require('koa-bodyparser')
+const Router = require('koa-router')
 const prepareDatabase = require('../database').prepareDatabase
 
-prepareDatabase('secret-password').then((database) => {
+prepareDatabase('$2b$10$8jyIo5qqXYKWEOjUc6SX3OFQ2BFpre9UyDuAjNfjqGybUAeP1kAJK').then((database) => {
   console.log('database', database)
-  database.each("SELECT * FROM users", function(err, row) {
-    console.log(row);
-  });
-  console.log('more')
+  const app = new Koa()
+  const router = new Router()
+
+  const requireAuthentication = (ctx, next) => {
+    if (ctx.session.authenticated) {
+      next()
+    } else {
+      ctx.redirect('/login')
+    }
+  }
+  router.get('/', requireAuthentication, (ctx) => {
+    ctx.type = 'html'
+    ctx.body = fs.createReadStream(path.join('level-1', 'client', 'index.html'))
+  })
+
+  router.get('/login', (ctx) => {
+    ctx.type = 'html'
+    ctx.body = fs.createReadStream(path.join('level-1', 'client', 'login.html'))
+  })
+
+  const authenticate = (ctx, user) => {
+    return new Promise((resolve, reject) => {
+      const statement = database.prepare('SELECT * FROM users WHERE username=?')
+      statement.run(user.username).all((err, rows) => {
+        if (err) throw err
+
+        console.log('rows', rows)
+        if (rows.length === 1) {
+          bcrypt.compare(user.password, rows[0].hash, (err, isSame) => {
+            if (err) throw err
+            console.log(isSame)
+            if (isSame) ctx.session.authenticated = true
+            resolve()
+          })
+        } else {
+          bcrypt.compare('jambolaia', '$2b$10$8jyIo5qqXYKWEOjUc6SX3OFQ2BFpre9UyDuAjNfjqGybUAeP1kAJK', () => {
+            resolve()
+          })
+        }
+      })
+    })
+  }
+
+  router.post('/login', async (ctx) => {
+    const user = ctx.request.body
+    if (user.username !== undefined && user.password !== undefined) {
+      await authenticate(ctx, user)
+    }
+
+    ctx.redirect('/')
+  })
+
+  app.keys = [Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 64)]
+  app.use(session(app))
+
+  app
+    .use(bodyParser())
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+  app.listen(3000)
 })
--- level-2/server.js
+++ level-2/server.js
@@ -1,1 +1,14 @@
-console.log('hello from server 2')
+console.log('hello from server 1')
+const forge = require('node-forge')
+const prepareDatabase = require('../database').prepareDatabase
+
+prepareDatabase('789c75724608ed258736322b0d780ca0b6ce23458610ec6687cfd471a34c8a5220d34a83248099c9815bcab86cb97b97c54331c10b23ef07251938ebf9cbac35').then((database) => {
+  console.log('database', database)
+  database.each("SELECT * FROM users", function(err, row) {
+    console.log(row);
+  });
+  const messageDigest = forge.md.sha512.create()
+  messageDigest.update('Strong87Secret09')
+  console.log(messageDigest.digest().toHex())
+  console.log('more')
+})
