e91067ae8c085765f60cd03cab98f404ed58cb97
ebakoba <kaarelallemann@gmail.com>
2018-11-12 13:39:06 +0200

    add client logging

--- websocketServer.js
+++ websocketServer.js
@@ -6,9 +6,21 @@ const { requireAuthentication } = require('./authentication')
 const app = websockify(new Koa())
 const websocketRouter = new Router()
 
+const activeClients = []
+
+const broadcastMessage = (message) => {
+  console.log(activeClients.length)
+  activeClients.forEach((client) => {
+    client.send(message)
+  })
+}
+
 websocketRouter.get('/websocket', (ctx) => {
   console.log('hello connection')
-  ctx.websocket.send('Hello world')
+
+  console.log('connection opened')
+  activeClients.push(ctx.websocket)
+  broadcastMessage('hello world')
   ctx.websocket.on('message', (message) => {
     console.log('message', message)
   })
