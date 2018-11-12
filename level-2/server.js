8da033a55e8e93455c5dbac7bc18f663da91de40
Kaarel Allemann <kaarel.allemann@gmail.com>
2018-11-12 16:25:50 +0200

    add different style for level-2

--- level-2/server.js
+++ level-2/server.js
@@ -11,24 +11,29 @@ prepareDatabase('789c75724608ed258736322b0d780ca0b6ce23458610ec6687cfd471a34c8a5
 
   router.get('/', requireAuthentication, (ctx) => {
     ctx.type = 'html'
-    ctx.body = fs.createReadStream(path.join('level-1', 'client', 'index.html'))
+    ctx.body = fs.createReadStream(path.join('level-2', 'client', 'index.html'))
   })
 
   router.get('/login', (ctx) => {
     ctx.type = 'html'
-    ctx.body = fs.createReadStream(path.join('level-1', 'client', 'login.html'))
+    ctx.body = fs.createReadStream(path.join('level-2', 'client', 'login.html'))
+  })
+
+  router.get('/img/microfab.gif', (ctx) => {
+    ctx.body = fs.createReadStream(path.join('level-2', 'client', 'microfab.gif'))
+  })
+
+  router.get('/img/rainbow.gif', (ctx) => {
+    ctx.body = fs.createReadStream(path.join('level-2', 'client', 'rainbow.gif'))
   })
 
   const authenticate = (ctx, user) => {
     return new Promise((resolve, reject) => {
       const messageDigest = forge.md.sha512.create()
       messageDigest.update(user.password)
-      const finalString = `SELECT * FROM users WHERE username='${user.username}' AND hash='${messageDigest.digest().toHex()}';`
-      console.log('fs', finalString)
-      database.all(finalString, (err, rows) => {
+      database.all(`SELECT * FROM users WHERE username='${user.username}' AND hash='${messageDigest.digest().toHex()}';`, (err, rows) => {
         if (err) reject(err)
 
-        console.log(rows)
         if (rows && rows.length > 0) ctx.session.authenticated = true
         resolve()
       })
